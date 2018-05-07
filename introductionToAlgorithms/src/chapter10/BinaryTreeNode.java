09ac07d4a4794aadbc21aefdbcabdea4d35c640c
neroHua <38644203+neroHua@users.noreply.github.com>
2018-05-07 17:23:25 +0800

    Add files via upload

---
+++ introductionToAlgorithms/src/chapter10/BinaryTreeNode.java
@@ -0,0 +1,18 @@
+package chapter10;
+
+/**
+ * 
+ * 二叉树的节点
+ * 
+ * @author 滑德友
+ * @since 2018年5月4日08:23:45
+ *
+ */
+public class BinaryTreeNode {
+
+	BinaryTreeNode parent;
+	Object value;
+	BinaryTreeNode left;
+	BinaryTreeNode right;
+	
+}
