7d22549af4884be9fd06d2adceffdf8f5e692f27
neroHua <38644203+neroHua@users.noreply.github.com>
2018-05-11 17:32:56 +0800

    Add files via upload

---
+++ introductionToAlgorithms/src/chapter13/RedBlackTreeNode.java
@@ -0,0 +1,20 @@
+package chapter13;
+
+/**
+ * 
+ * 红黑树的节点
+ * 
+ * @author 滑德友
+ * @since 2018年5月10日10:55:48
+ *
+ */
+public class RedBlackTreeNode {
+
+	RedBlackTreeNode parent;
+	RedBlackTreeNode left;
+	RedBlackTreeNode right;
+	int value;
+
+	int color;
+
+}
